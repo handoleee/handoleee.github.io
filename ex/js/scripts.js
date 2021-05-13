4925760b22e59e1118a1c318a9bc311af3c06cbf
hansol <p0603hs@naver.com>
2021-05-13 11:41:57 +0900

    change

--- ex/js/scripts.js
+++ ex/js/scripts.js
@@ -3,6 +3,7 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
+
 (function ($) {
     "use strict"; // Start of use strict 
 
